920fdec99726947f0e4547dd8f9c29c3617c5591
Umit Karaduman <143025931+umtkrdmn@users.noreply.github.com>
2024-03-27 09:17:41 -0700

    Add files via upload
    Init

---
+++ uploader.sh
@@ -0,0 +1,3 @@
+git pull
+java -DSERVER_URL=https://airth-api-gateway.dev.airth.io/api/agent -DAUTH_SERVER_URL=https://airth-api-gateway.dev.airth.io/api/auth -jar S3FileUploaderDesktop-1.0-SNAPSHOT-jar-with-dependencies.jar&
+
